90c0572728f10ad2048c3023e234d8e4e8aae822
Yung Dai <yungchidai@gmail.com>
2015-04-12 14:07:00 -0400

    Initial commit

---
+++ SimpleTable/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  SimpleTable
+//
+//  Created by Yung Dai on 2015-04-12.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+
+
+@end
+
---
+++ SimpleTable/ViewController.m
@@ -0,0 +1,46 @@
+//
+//  ViewController.m
+//  SimpleTable
+//
+//  Created by Yung Dai on 2015-04-12.
+//  Copyright (c) 2015 Yung Dai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+{
+    NSArray *recipes;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell  = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"creme_brelee"];
+    
+    return cell;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Initialize table data
+    recipes = @[@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
